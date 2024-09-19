d088491e34e053059dbbf038741d54b48f666f6e
Victor Moreira <zvihugo@hotmail.com>
2024-09-19 08:20:50 -0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
 import { Card } from "./components/Card/Card";
+import Header from "./components/Header/Header"
 
 function App() {
   return (
     <>
+      <Header/>
       <Card/>
     </>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
-
+import linux from "../../images/Linux.png";
+import { Box, ChakraProvider, Image, Heading } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <div>
-        <h1>Linux bank</h1>
-    </div>
-  )
+    <ChakraProvider>
+      <Box backgroundColor="black" display="flex" alignItems="center" padding="20px">
+        <Image src={linux} alt="Linux Pinguim" marginRight="20px" height="100px" />
+        <Heading as="h1" color="white" textAlign="center" flex="1">
+          bank
+        </Heading>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
-export default Header
+export default Header;
